90b72fd8d4806e7f0b238fe893af9823869ae94d
Santiago Manjarin <santiagomanjarin111@gmail.com>
2026-03-12 18:35:13 -0300

    Agregado de libreria de tableros react-chessboard. Front super pelado con css simple agregado

--- web/frontend/src/App.tsx
+++ web/frontend/src/App.tsx
@@ -1,79 +1,35 @@
 import { useEffect, useState } from "react";
+import { Chessboard } from "react-chessboard";
 import "./App.css";
-import reactLogo from "./assets/react.svg";
 import createEngineModule from "./wasm/engine.js";
-import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0);
   const [wasmLoaded, setWasmLoaded] = useState(false);
   const [sumResult, setSumResult] = useState<number | null>(null);
-
+  const chessboardOptions = {};
   useEffect(() => {
-    let cancelled = false;
-
-    (async () => {
-      try {
-        const Module = await createEngineModule({
-          locateFile: (path: string) => {
-            if (path.endsWith(".wasm")) {
-              return "/src/wasm/engine.wasm";
-            }
-            return path;
-          },
-        });
-
-        if (cancelled) return;
-
-        const s = Module.add(2, 3);
-        setSumResult(s);
-
-        const engineInstance = new Module.Engine();
-        engineInstance.isEngineWorking();
-        console.log("wrapper add:", engineInstance.add(4, 5));
-
-        setWasmLoaded(true);
-      } catch (err) {
-        console.error("WASM load error:", err);
-      }
-    })();
-
-    return () => {
-      cancelled = true;
+    const initWasm = async () => {
+      const { instance } = await createEngineModule();
+      setWasmLoaded(true);
+      setSumResult(instance.exports.add(2, 3));
     };
+    initWasm();
   }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <h1>Vite + React</h1>
-
-      <div className="card">
-        <button onClick={() => setCount((c) => c + 1)}>count is {count}</button>
-
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-
-        <p>
-          {wasmLoaded
-            ? `WASM ready — add(2,3) = ${sumResult}`
+    <div id="app">
+      <div id="header">
+        <h1 id="header-title">Chess engine</h1>
+        <p id="header-wasm-info">
+          {wasmLoaded && sumResult !== null
+            ? `Wasm in working`
             : "Cargando WASM..."}
         </p>
       </div>
-
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <div className="chessboard">
+        <Chessboard options={chessboardOptions} />
+      </div>
+    </div>
   );
 }
 
